970603e06acea3e12662710d70150e5572531a3e
Mike Hodges <mike_hodges44@yahoo.com>
2016-10-17 13:48:02 -0600

    Added AngularJS Support

--- script.js
+++ script.js
@@ -1,8 +1,59 @@
-$(function() {
-    $("a.kt-action-button").on("click", function () {
-       alert("all done!"); 
+var app = angular.module("myApp", [])
+    .directive("keyTrapper", function() {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                element.keyTrapper({
+                    escape: [27, 123], // enter, f12
+                    trapKeys: [103, 105], // numPad7, numPad9
+                    onActionButton: function() {
+                        var finished = confirm("Are you sure you want to submit the angular data?");
+                        if (finished) {
+                            this.actionButton.trigger("click");
+                        }
+                    }
+                });
+            }
+        };
+    })
+    .controller("myCtrl", function($scope) {
+        $scope.containers = [1,2,3];
+        $scope.formControls = [{
+            name: "ctrl1",
+            value: "",
+            type: "text",
+            id: "ctrl1",
+            ignore: false
+        }, {
+            name: "ctrl2",
+            value: "",
+            type: "checkbox",
+            id: "ctrl2",
+            ignore: false
+        }, {
+            name: "ctrl3",
+            value: "",
+            type: "text",
+            id: "ctrl3",
+            ignore: true
+        }, {
+            name: "ctrl4",
+            value: "",
+            type: "text",
+            id: "ctrl4",
+            ignore: false
+        }];
+
+        $scope.sendMessage = function() {
+            alert("All done, angular!");
+        };
     });
-    $(".kt-container:not(.container-one, .container-two)").keyTrapper({
+
+$(function() {
+    $("a.kt-action-button").on("click", function() {
+        alert("all done!");
+    });
+    $(".kt-container:not(.container-one, .container-two):not([key-trapper])").keyTrapper({
         escape: [27, 123], // enter, f12
         trapKeys: [8, 97], // backspace, numPad1
         onActionButton: function() {
